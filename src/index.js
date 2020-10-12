f29613a432e7109ad8fba5a5f39c60e432474d4b
gtycbjyth <kovaltru@gmail.com>
2020-10-12 17:31:22 +0300

    fox:complete task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,24 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    const m = [ ];
+    if (matrix === undefined) {
+        return m;
+    } 
+
+    for (let i = 0; i<matrix.length; i++ ){
+        let arr = matrix[i];
+        if (i%2 == 0) {
+            for (let j = 0; j<arr.length; j++){
+                m.push(arr[j]);
+            } 
+        }else {
+            arr = arr.reverse();
+            for (let j = 0; j<arr.length; j++){
+                m.push(arr[j]);      
+            }          
+        }        
+    }
+    return m;
 }
+
